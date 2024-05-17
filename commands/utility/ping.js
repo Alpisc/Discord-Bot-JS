4936447cbdeb862e1e2227dc08ff8eaa4482a6dc
Alpisc <alpisc@duck.com>
2024-05-17 13:19:30 +0200

    removed cooldown and improved commands

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,11 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Displays the Bot\'s latency.'),
 	async execute(interaction) {
-		await interaction.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+		await interaction.reply(`🏓Latency is ${Math.abs(Date.now() - interaction.createdTimestamp)}ms.`);
 	},
 };
