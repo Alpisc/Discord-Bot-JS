4ff9be520bc3cbff84075f53139618bd5a4614d6
Alpisc <alpisc@duck.com>
2024-05-17 14:01:54 +0200

    fixed move

--- commands/utility/move.js
+++ commands/utility/move.js
@@ -1,34 +1,53 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('move')
-		.setDescription('Moves users from one channel to another.')
+    data: new SlashCommandBuilder()
+        .setName('move')
+        .setDescription('Moves users from one voice channel to another.')
         .addChannelOption(option =>
             option
-            .setName("from")
-            .setDescription("From where do you want to move the users?")
-            .setRequired(true)
+                .setName("from")
+                .setDescription("From where do you want to move the users?")
+                .setRequired(true)
+                .addChannelTypes([ChannelType.GuildVoice]) // Restrict to voice channels
         )
         .addChannelOption(option =>
             option
-            .setName("to")
-            .setDescription("Where do you want to move the users to?")
-            .setRequired(true)
+                .setName("to")
+                .setDescription("Where do you want to move the users to?")
+                .setRequired(true)
+                .addChannelTypes([ChannelType.GuildVoice]) // Restrict to voice channels
         )
         .setDefaultMemberPermissions(PermissionFlagsBits.MoveMembers),
-	async execute(interaction) {
-        let from = interaction.options.getString('from');
-        let to = interaction.options.getString('to');
-        let counter = 0;
-        if(from == to) return;
-        from.members.forEach((member) => {
-            if(member.voice.channel){
-                member.voice.setChannel(to);
-                counter++;
+        async execute(interaction) {
+            const from = interaction.options.getChannel('from');
+            const to = interaction.options.getChannel('to');
+        
+            // Ensure both channels are voice channels
+            if (from.type !== ChannelType.GuildVoice || to.type !== ChannelType.GuildVoice) {
+                return interaction.reply({ content: "Both channels must be voice channels.", ephemeral: true });
             }
-        });
-
-		await interaction.send(`Moved ${counter} users from <#${from.id}> to <#${to.id}>`);
-	},
+        
+            // Prevent moving users to the same channel
+            if (from.id === to.id) {
+                return interaction.reply({ content: "You cannot move users to the same channel.", ephemeral: true });
+            }
+        
+            let counter = 0;
+        
+            // Check if there are members in the 'from' channel
+            if (from.members.size === 0) {
+                return interaction.reply({ content: "There are no users in the voice channel to move.", ephemeral: true });
+            }
+        
+            // Move members from 'from' channel to 'to' channel
+            from.members.forEach(member => {
+                if (member.voice.channel) {
+                    member.voice.setChannel(to);
+                    counter++;
+                }
+            });
+        
+            await interaction.reply(`Moved ${counter} users from <#${from.id}> to <#${to.id}>`);
+        },
 };
