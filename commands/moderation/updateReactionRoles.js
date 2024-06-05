cf035c80e94917283819df0b41a0ac030995e824
Alpisc <Alpisc@duck.com>
2024-06-05 13:48:07 +0200

    added refresh reaction roles (so you dont have to run the file)

---
+++ commands/moderation/updateReactionRoles.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const roles = require("../../roles.json");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('updatereactionroles')
+		.setDescription('Updates the reaction roles')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+	async execute(interaction) {
+        const channel = await client.channels.cache.get(process.env.reactionChannelId);
+        if (!channel) return;
+    
+        const rows = [];
+        let row = new ActionRowBuilder();
+    
+        roles.forEach((role, index) => {
+            if (index % 5 === 0 && index !== 0) { // Every 5 entries, start a new row
+                rows.push(row);
+                row = new ActionRowBuilder();
+            }
+            row.addComponents(
+                new ButtonBuilder().setCustomId(role.id).setLabel(role.label).setStyle(ButtonStyle.Primary)
+            );
+        });
+    
+        rows.push(row); // Push the last row even if it's not full
+    
+        await channel.bulkDelete(1);
+    
+        await channel.send({ content: "Claim or remove a role", components: rows });
+    }
+};
