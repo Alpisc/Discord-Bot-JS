df0b909d8f9478f6129d1dcb45b35696736a9d7a
Alpisc <alpisc@duck.com>
2024-07-03 13:58:51 +0200

    added missing require

--- commands/moderation/addReactionRole.js
+++ commands/moderation/addReactionRole.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const fs = require("fs");
+const path = require("path");
 
 module.exports = {
 	data: new SlashCommandBuilder()
