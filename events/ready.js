16cb2acf35ea41dd4ef1d0b636c8117d61d58145
Alpisc <alpisc@duck.com>
2024-07-10 22:53:22 +0200

    updated roles location

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,7 @@ const path = require('node:path');
 const fs = require('node:fs');
 const sendReactionRole = require("../functions/sendReactionRole");
 
+
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
@@ -10,7 +11,7 @@ module.exports = {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
         await client.user.setPresence({ activities: [{ name: 'You', type: ActivityType.Watching }], status: 'dnd' });
 
-		const rolesPath = path.join(__dirname, "./roles.json");
+		const rolesPath = path.join(__dirname, "../roles.json");
 		const roles = JSON.parse(fs.readFileSync(rolesPath, "utf-8"));
 	
 		await sendReactionRole(client, roles);
