46da90017b47753258328dfea81f1fd47a393f17
Alpisc <alpisc@duck.com>
2024-07-14 00:36:49 +0200

    prevent bot crashing from "unkown interaction"

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -42,10 +42,14 @@ module.exports = {
 			await command.execute(interaction);
 		} catch (error) {
 			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			try {
+				if (interaction.replied || interaction.deferred) {
+					await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+				} else {
+					await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+				}
+			} catch (followUpError) {
+				console.error('Failed to send error message:', followUpError);
 			}
 		}
 	},
