e577792e20410a84f287539bf156c66b16a27780
Alpisc <alpisc@duck.com>
2024-10-04 23:12:22 +0200

    fix mute commands

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -3,22 +3,29 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('mute')
-		.setDescription('Mutes a specific User')
+		.setDescription('Mutes a specific User in a text channel')
         .addUserOption(option =>
             option
                 .setName("user")
                 .setDescription("Who do you want to mute")
                 .setRequired(true)
         )
+        .addChannelOption(option =>
+            option
+                .setName("channel")
+                .setDescription("The channel to mute the user in")
+                .setRequired(true)
+        )
         .setDefaultMemberPermissions(PermissionFlagsBits.MuteMembers),
 	async execute(interaction) {
-        const user = interaction.options.getMember("user"); // Use getMember instead of getUser
-        
+        const user = interaction.options.getMember("user"); // Use getMember to get the GuildMember object
+        const channel = interaction.options.getChannel("channel"); // Get the specified channel
+
         if(user.id === interaction.member.id){
             return interaction.reply({ content: "You can't mute yourself.", ephemeral: true });
         }
 
-        await user.voice.setMute(true); // Use setMute method on the GuildMember's voice state
-        await interaction.reply({content: `Muted ${user}.`});
+        await channel.permissionOverwrites.edit(user, { SEND_MESSAGES: false }); // Update permissions to mute the user in the text channel
+        await interaction.reply({content: `Muted ${user} in ${channel}.`});
     }
 };
--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -3,22 +3,29 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('unmute')
-		.setDescription('Unmutes a specific User')
+		.setDescription('Unmutes a specific User in a text channel')
         .addUserOption(option =>
             option
                 .setName("user")
                 .setDescription("Who do you want to unmute")
                 .setRequired(true)
         )
+        .addChannelOption(option =>
+            option
+                .setName("channel")
+                .setDescription("The channel to unmute the user in")
+                .setRequired(true)
+        )
         .setDefaultMemberPermissions(PermissionFlagsBits.MuteMembers),
 	async execute(interaction) {
-        const user = interaction.options.getUser("user");
-        
-        if(user == interaction.member){
-            return interaction.reply({ content: "You cant unmute yourself.", ephemeral: true }); //propapy not needed since you cant write
+        const user = interaction.options.getMember("user"); // Use getMember to get the GuildMember object
+        const channel = interaction.options.getChannel("channel"); // Get the specified channel
+
+        if(user.id === interaction.member.id){
+            return interaction.reply({ content: "You can't unmute yourself.", ephemeral: true });
         }
 
-        await user.edit({mute: true});
-        await interaction.reply({content: `Unmuted ${user}.`});
+        await channel.permissionOverwrites.edit(user, { SEND_MESSAGES: null }); // Update permissions to unmute the user in the text channel
+        await interaction.reply({content: `Unmuted ${user} in ${channel}.`});
     }
 };
