6dc5ab4faf12c5940c9e94a9bf30ffc1bcc6fa52
Alpisc <alpisc@duck.com>
2025-08-03 14:06:31 +0200

    add ping option

--- commands/moderation/announce.js
+++ commands/moderation/announce.js
@@ -10,12 +10,17 @@ module.exports = {
             .setDescription("The announcement you want to send")
             .setRequired(true)
         )
+        .addBooleanOption(option =>
+            option.setName('ping')
+            .setDescription('Whether or not @everyone should be pinged')
+        )
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction) {
         const announcement = interaction.options.getString("announcement");
+        const ping = interaction.options.getBoolean('ping');
 
-        await interaction.client.channels.cache.get(process.env.announcementChannelId).send({ content: `# 🔔🔔 Annoucement 🔔🔔\n\n${announcement}` });
-        await interaction.reply({content: `Send announcement in <#${process.env.announcementChannelId}>.`, ephemeral: true});
+        await interaction.client.channels.cache.get(process.env.announcementChannelId).send({ content: `# 🔔🔔 Annoucement 🔔🔔\n\n${announcement}${ping ? "\n\n||@everyone||" : ""}` });
+        await interaction.reply({content: `Send announcement in <#${process.env.announcementChannelId}>${ping ? " and pinged everyone" : ""}.`, ephemeral: true});
     }
 };
 
