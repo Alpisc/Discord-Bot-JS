b489cccabb9c94d111ab9c8d82ba656e9a080904
Alpisc <alpisc@duck.com>
2025-08-03 13:56:04 +0200

    fix command

--- commands/moderation/announce.js
+++ commands/moderation/announce.js
@@ -13,8 +13,8 @@ module.exports = {
 	async execute(interaction) {
         const announcement = interaction.options.getString("announcement");
 
-        await interaction.client.channels.cache.get(process.env.announcementChannelId).send({ content: `🔔🔔 Annoucement 🔔🔔\n\n${announcement}` });
-        await interaction.reply({content: `Muted ${user} in all channels.`, ephemeral: true});
+        await interaction.client.channels.cache.get(process.env.announcementChannelId).send({ content: `# 🔔🔔 Annoucement 🔔🔔\n\n${announcement}` });
+        await interaction.reply({content: `Send announcement in <#${process.env.announcementChannelId}>.`, ephemeral: true});
     }
 };
 
